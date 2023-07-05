123f4d5e6bfc28ee26ad9855c28e61a76d821951
Aswanidevm <maswanidev@gmail.com>
2023-07-05 09:41:17 +0530

    ff

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_iam_instance_profile" "instance_profile" {
 }
 
 resource "aws_iam_role_policy_attachment" "policy-attach" {
-  role       = [aws_iam_role.role.name]
+  role       = aws_iam_role.role.name
   policy_arn = aws_iam_policy.policy.arn
 }
 
